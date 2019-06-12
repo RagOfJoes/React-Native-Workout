2c1bc893abb1bed4b81490a00b00550024a7f6c0
RagOfJoes <vhsvragojos@gmail.com>
2019-06-12 01:05:46 -0700

    Created Current Routine Section

---
+++ Components/Home/Current Routine/CurrentRoutine.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CurrentRoutine = (props) => {
+    return (
+        <View style={styles.currentRoutineContainer}>
+            <Text style={styles.currentRoutineText}>
+                CURRENT ROUTINE
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    currentRoutineContainer: {
+        height: 170,
+        width: "80%",
+        marginTop: 40,
+        alignSelf: "center",
+        // backgroundColor: "red",
+        flexDirection: "column",
+    },
+    currentRoutineText: {
+        fontSize: 14,
+        color: "#313030",
+        borderBottomWidth: .75,
+        fontFamily: "Roboto-Medium",
+        borderBottomColor: '#D2D2D2',
+    }
+})
+
+export default CurrentRoutine;
