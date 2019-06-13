b2915578f3f61cc2e12c80b17f1af3aa3b25bc88
RagOfJoes <vhsvragojos@gmail.com>
2019-06-13 15:45:04 -0700

    created otherRoutines section

--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import NextWorkout from './Next Workout/NextWorkout';
+import OtherRoutines from './Other Routines/OtherRoutines';
 import CurrentRoutine from './Current Routine/CurrentRoutine';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
@@ -10,6 +11,7 @@ const Home = () => {
             <Header />
             <NextWorkout />
             <CurrentRoutine />
+            <OtherRoutines />
         </View>
     )
 }
---
+++ Components/Home/Other Routines/OtherRoutines.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const OtherRoutines = (props) => {
+    return (
+        <View style={styles.otherRoutineContainer}>
+            <View style={{ width: "80%", height: "100%" }}>
+                <View style={styles.otherRoutineTitleRow}>
+                    <Text style={styles.otherRoutineTextOne}>OTHER ROUTINES</Text>
+                    <TouchableOpacity>
+                        <Text style={styles.otherRoutineTextTwo}>SEE ALL</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.otherRoutineCardsRow}>
+
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    otherRoutineContainer: {
+        height: 140,
+        width: "100%",
+        marginTop: 40,
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundColor: "#FFF"
+    },
+    otherRoutineTitleRow: {
+        flex: .5,
+        flexDirection: 'row'
+    },
+    otherRoutineTextOne: {
+        fontSize: 14,
+        color: "#313030",
+        marginRight: "auto",
+        fontFamily: "Roboto-Medium"
+    },
+    otherRoutineTextTwo: {
+        fontSize: 10,
+        color: "#828282",
+        marginTop: "auto",
+        fontFamily: "Roboto-Regular"
+    },
+    otherRoutineCardsRow: {
+        flex: 3.5,
+    }
+})
+
+export default OtherRoutines;
