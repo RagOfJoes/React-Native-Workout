374626cea6368b9a76a68202b9575a92ec811a1e
RagOfJoes <vhsvragojos@gmail.com>
2019-06-16 16:33:12 -0700

    added color config file

---
+++ Components/config/colors.js
@@ -0,0 +1,8 @@
+export const color = {
+    PRIMARY_DARK: "#3E4047",
+    SECONDARY_DARK: "#474B54",
+    TERTIARY_DARK: "#5E6473",
+    WHITE: "#E1E1E1",
+    GREY: "#828282",
+    HIGHLIGHT: "#F9C56D",
+}
