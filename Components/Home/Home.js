3f5c622bb27e2507934cb8d832f0cdcd3d196d3b
RagOfJoes <vhsvragojos@gmail.com>
2019-06-17 06:15:28 -0700

    prettified code

--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import { color } from '../config/colors';
 import NextWorkout from './Next Workout/NextWorkout';
@@ -6,23 +6,25 @@ import OtherRoutines from './Other Routines/OtherRoutines';
 import CurrentRoutine from './Current Routine/CurrentRoutine';
 import { View, StyleSheet } from 'react-native';
 
-const Home = () => {
-    return (
-        <View style={styles.container}>
-            <Header />
-            <NextWorkout />
-            <CurrentRoutine />
-            <OtherRoutines />
-        </View>
-    )
+class Home extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header />
+                <NextWorkout />
+                <CurrentRoutine />
+                <OtherRoutines />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        backgroundColor: color.PRIMARY_DARK,
         justifyContent: "space-evenly",
+        backgroundColor: color.PRIMARY_DARK,
     }
 })
 
