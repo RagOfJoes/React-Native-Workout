42380e77d23e1c1b2aa6c80a358e9095ae486e50
RagOfJoes <vhsvragojos@gmail.com>
2019-06-17 06:15:18 -0700

    converted size to flex and normalized fontSzie

--- Components/Home/Header.js
+++ Components/Home/Header.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { color } from '../config/colors';
 import { fontSize } from '../config/fontSize';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-import normalizeText from '../config/normalizeText';
+import { View, Text, Image, Platform, TouchableOpacity, StyleSheet } from 'react-native';
 
 // TODO: addWorkout should be using drawable folders
 // create action for addWorkout(on press change page or open up a modal to create a new workout plan)
 const Header = () => {
     return (
-        <View style={styles.header}>
-            <Text style={[fontSize.PAGE_TITLE, styles.title]}>WORKOUTS</Text>
-            <TouchableOpacity style={styles.addWorkout} onPress={() => console.log("Pressed")}>
-                <Image style={{ width: 30, height: 30 }} resizeMode="contain" source={require("../../assets/addWorkout.png")} />
-            </TouchableOpacity>
+        <View style={styles.headerContainer}>
+            <View style={styles.header}>
+                <Text style={[fontSize.PAGE_TITLE, styles.title]}>WORKOUTS</Text>
+                <TouchableOpacity style={styles.addWorkout} onPress={() => console.log("Pressed")}>
+                    <Image style={{ width: 30, height: 30 }} resizeMode="contain" source={require("../../assets/addWorkout.png")} />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    headerContainer: {
+        flex: .15,
+        maxHeight: 95,
+        alignItems: "center",
+        justifyContent: "center",
+    },
     header: {
-        width: '80%',
-        height: "auto",
-        alignSelf: 'center',
-        flexDirection: 'row',
+        width: "90%",
+        flexDirection: "row",
         borderBottomWidth: .75,
         borderBottomColor: color.HIGHLIGHT,
     },
