480f51662e6f8b1f4193c6f3c30e4a2fdef2057e
RagOfJoes <vhsvragojos@gmail.com>
2019-06-18 15:20:19 -0700

    removed prop navigation to header

--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,22 +1,20 @@
-import Header from './Header';
-import React, { Component } from 'react';
+import React from 'react';
+import Header from './Sections/Header';
 import { color } from '../config/colors';
 import { View, StyleSheet } from 'react-native';
-import NextWorkout from './Next Workout/NextWorkout';
-import OtherRoutines from './Other Routines/OtherRoutines';
-import CurrentRoutine from './Current Routine/CurrentRoutine';
+import NextWorkout from './Sections/Next Workout/NextWorkout';
+import OtherRoutines from './Sections/Other Routines/OtherRoutines';
+import CurrentRoutine from './Sections/Current Routine/CurrentRoutine';
 
-class Home extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Header navigation={this.props.navigation} />
-                <NextWorkout />
-                <CurrentRoutine />
-                <OtherRoutines />
-            </View>
-        )
-    }
+const Home = (props) => {
+    return (
+        <View style={styles.container}>
+            <Header />
+            <NextWorkout />
+            <CurrentRoutine />
+            <OtherRoutines />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
