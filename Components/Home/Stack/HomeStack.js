07f6cf2e0d5fa76b7013165818072b5b037f99ab
Victor Ragojos <victorragojos@victors-mbp.attlocal.net>
2019-06-19 07:31:41 -0700

    transferred Home Header style to react-nav

--- Components/Home/Stack/HomeStack.js
+++ Components/Home/Stack/HomeStack.js
@@ -1,24 +1,47 @@
+import React from 'react';
 import Home from '../Home';
 import { color } from '../../config/colors';
 import { fontSize } from '../../config/fontSize';
 import NewRoutine from '../Pages/New Routine/NewRoutine';
 import { createStackNavigator } from 'react-navigation';
+import { Text, Image, TouchableOpacity } from 'react-native';
 
 const HomeStack = createStackNavigator(
     {
         Home: {
             screen: Home,
-            navigationOptions: {
+            navigationOptions: ({ navigation }) => ({
+                headerMode: "none",
                 headerStyle: {
-                    display: "none"
-                }
-            }
+                    width: "90%",
+                    alignSelf: "center",
+                    borderBottomWidth: .75,
+                    borderBottomColor: color.HIGHLIGHT,
+                    backgroundColor: color.PRIMARY_DARK
+                },
+                headerLeft: <Text style={[fontSize.PAGE_TITLE, {
+                    color: color.WHITE,
+                    marginRight: 'auto',
+                    alignSelf: 'center',
+                }]}>WORKOUTS</Text>,
+                headerRight: (
+                    <TouchableOpacity style={{
+                        width: 30,
+                        height: 30,
+                        alignSelf: 'center'
+                    }} onPress={() => navigation.navigate("NewRoutine")}>
+                        <Image style={{ width: 30, height: 30 }} resizeMode="contain" source={require("../../../assets/addWorkout.png")} />
+                    </TouchableOpacity>
+                )
+            })
         },
         NewRoutine: {
             screen: NewRoutine,
             navigationOptions: {
                 title: "NEW ROUTINE",
+                headerMode: "none",
                 headerStyle: {
+                    borderBottomWidth: 0,
                     backgroundColor: color.SECONDARY_DARK
                 },
                 headerTintColor: color.WHITE,
@@ -33,7 +56,7 @@ const HomeStack = createStackNavigator(
     },
     {
         // TODO: Change back to home
-        initialRouteName: "NewRoutine",
+        initialRouteName: "Home",
     }
 );
 
