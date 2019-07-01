c8ebaaadbb94cf93a472820ac24fd691c3e064a0
Victor Ragojos <victorragojos@victors-mbp.attlocal.net>
2019-06-30 22:19:54 -0700

    now utlizes WorkoutCard component

--- Components/Home/Sections/Next Workout/NextWorkout.js
+++ Components/Home/Sections/Next Workout/NextWorkout.js
@@ -1,152 +1,49 @@
-import Muscle from '../../../Views/Muscle';
-import React, { Component } from 'react';
-import { color } from '../../../config/colors';
-import { fontSize } from '../../../config/fontSize';
+import React from 'react';
+import { View } from 'react-native';
+import WorkoutCard from '../../Pages/New Routine/Workouts/WorkoutCard';
 
-import { View, Text, FlatList, Dimensions, StyleSheet } from 'react-native';
-
-// TODO: Fetch current routine and the day's planned workout
-// 
-const data = ["Chest", "Shoulders", "Legs", "Arms", "Back"];
-
-const formatData = (data, numColumns) => {
-    let numberOfElements = data.length;
-    while (numberOfElements != 6) {
-        numberOfElements++;
-        if (numberOfElements !== 6) {
-            data.push("Empty");
-        } else {
-            data.push("Last");
-        }
+const data = [
+    {
+        Name: "Push",
+        Exercises: [
+            {
+                Type: "Chest",
+                Name: "Bench Press"
+            },
+            {
+                Type: "Shoulders",
+                Name: "Military Press"
+            },
+            // {
+            //     Type: "Chest",
+            //     Name: "Incline Bench Press"
+            // },
+            // {
+            //     Type: "Chest",
+            //     Name: "Landmine Press"
+            // },
+            // {
+            //     Type: "Chest",
+            //     Name: "Dips"
+            // },
+            // {
+            //     Type: "Chest",
+            //     Name: "Chest Flyes"
+            // },
+            // {
+            //     Type: "Arms",
+            //     Name: "Tricep Extensions"
+            // }
+        ]
     }
+];
 
-    return data;
-};
-
-// TODO: Make this card a general component
-class NextWorkout extends Component {
-    _keyExtractor = (index) => {
-        return index
-    };
-
-    render() {
-        return (
-            <View style={{ flex: .5, overflow: "hidden", justifyContent: "center" }}>
-                <View style={styles.nextWorkoutContainerStyle}>
-                    <View style={styles.nextWorkoutBackgroundStyle}>
-                        {/* Start Workout Card */}
-                        <View style={styles.nextWorkoutRow}>
-                            {/* Start Workout Title */}
-                            <View style={styles.nextWorkoutTitle}>
-                                <Text style={[fontSize.CARD_TITLE, styles.title]} numberOfLines={1}>
-                                    Next Workout
-                                </Text>
-                                <Text style={[fontSize.CARD_CONTENT, styles.currentWorkout]} numberOfLines={3} adjustsFontSizeToFit>
-                                    pull a
-                                </Text>
-                            </View>
-                            {/* End Workout Title */}
-
-                            {/* Start Muscle Groups */}
-                            <FlatList
-                                numColumns="3"
-                                scrollEnabled={false}
-                                data={formatData(data, 3)}
-                                style={styles.muscleGroup}
-                                keyExtractor={this._keyExtractor}
-                                contentContainerStyle={{ justifyContent: "center", alignContent: "center", padding: 10 }}
-                                renderItem={({ item, index }) => { return (<Muscle key={index} containerStyle={styles.muscleContainer} muscleName={item} />) }}
-                            />
-                            {/* End Muscle Groups */}
-                        </View>
-                        {/* End Workout Card */}
-                    </View>
-                </View>
-            </View>
-        )
-    }
+const NextWorkout = (props) => {
+    return (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+            <WorkoutCard workoutName={data[0].Name} exercises={data[0].Exercises} isEditing={false} />
+        </View>
+    )
 }
-
-const styles = StyleSheet.create({
-    nextWorkoutContainerStyle: {
-        height: "70%",
-        width: '100%',
-        alignItems: "center"
-    },
-    nextWorkoutBackgroundStyle: {
-        width: "90%",
-        height: "100%",
-        borderRadius: 5,
-        alignItems: "flex-start",
-        flexDirection: 'column',
-        backgroundColor: color.SECONDARY_DARK,
-
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowOpacity: 0.29,
-        shadowRadius: 4.65,
-
-        elevation: 7,
-    },
-    nextWorkoutRow: {
-        height: "100%",
-        flexDirection: "row",
-        alignContent: "center",
-        justifyContent: "center"
-    },
-    // Start Title
-    nextWorkoutTitle: {
-        flex: .5,
-        maxWidth: 115,
-        maxHeight: "100%",
-        textAlign: "center",
-        alignItems: "center",
-        borderTopLeftRadius: 5,
-        justifyContent: "center",
-        borderBottomLeftRadius: 5,
-        backgroundColor: color.TERTIARY_DARK,
-    },
-    title: {
-        color: color.GREY,
-        textAlign: "center",
-        textTransform: "uppercase",
-    },
-    currentWorkout: {
-        color: color.WHITE,
-        textAlign: "center",
-        textTransform: "uppercase",
-    },
-    // End Title
-
-    // Start Muscle Group
-    muscleContainer: {
-        flex: 1,
-        margin: 2,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: color.TERTIARY_DARK,
-        height: Dimensions.get("screen").width / 5.75, // approximate a square
-
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-
-        elevation: 5,
-    },
-    muscleGroup: {
-        flex: 1,
-        width: "100%",
-        alignSelf: "center",
-    }
-    // End Muscle Group
-})
 
 export default NextWorkout;
