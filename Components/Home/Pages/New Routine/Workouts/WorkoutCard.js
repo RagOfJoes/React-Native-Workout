3d466fdaddd15cb42080030a9b44ebebb7d4e6d7
Victor Ragojos <victorragojos@victors-mbp.attlocal.net>
2019-06-30 22:17:27 -0700

    optimized formatData | created icons for buttons

--- Components/Home/Pages/New Routine/Workouts/WorkoutCard.js
+++ Components/Home/Pages/New Routine/Workouts/WorkoutCard.js
@@ -1,52 +1,46 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import Card from '../../../../Views/Card';
 import Muscle from '../../../../Views/Muscle';
 import { color } from '../../../../config/colors';
 import { fontSize } from '../../../../config/fontSize';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 import { View, Text, FlatList, Animated, TouchableOpacity, Dimensions, TextInput, StyleSheet } from 'react-native';
 
-const DeleteAction = ({ progress, dragX }) => {
-    const scale = dragX.interpolate({
-        inputRange: [0, 60],
-        outputRange: [0, 1],
-        extrapolate: "clamp"
-    });
+const templateData = {
+    Name: "",
+    Exercises: []
+}
 
+const DeleteAction = ({ progress, dragX }) => {
     const opacity = dragX.interpolate({
-        inputRange: [0, 80],
+        inputRange: [0, 65],
         outputRange: [0, 1]
     })
 
     return (
         <Animated.View style={[{ opacity: opacity }, styles.deleteWorkoutRow]}>
-            <TouchableOpacity style={{ width: "100%", height: "100%", alignContent: "center", justifyContent: "center" }}>
-                <Animated.Text style={[fontSize.CARD_TITLE, { color: color.WHITE, textAlign: "center", transform: [{ scale: scale }] }]}>DELETE</Animated.Text>
+            <TouchableOpacity style={{ flex: 1, height: "70%", alignContent: "center", backgroundColor: color.RED, justifyContent: "center" }}>
+                {/* <Text style={[fontSize.CARD_CONTENT, { color: color.WHITE, textAlign: "center" }]}>DELETE</Text> */}
+                <Text style={{ fontSize: 35, color: color.WHITE, textAlign: "center" }}>ⅹ</Text>
             </TouchableOpacity>
         </Animated.View>
     )
 }
 
-const RightAction = ({ progress, dragX }) => {
-    const scale = dragX.interpolate({
-        inputRange: [-60, 0],
-        outputRange: [1, 0],
-        extrapolate: "clamp"
-    });
-
+const RightAction = ({ progress, dragX, _pressEdit, _pressAdd }) => {
     const opacity = dragX.interpolate({
-        inputRange: [-60, 0],
+        inputRange: [-65, 0],
         outputRange: [1, 0]
     })
-
     return (
         <Animated.View style={[{ opacity: opacity }, styles.rightActionsRow]}>
-            <TouchableOpacity style={{ flex: 1, height: "100%", justifyContent: "center", backgroundColor: color.HIGHLIGHT }}>
-                <Animated.Text style={[fontSize.CARD_TITLE, { color: color.TERTIARY_DARK, textAlign: "center", transform: [{ scale: scale }] }]}>EDIT</Animated.Text>
+            <TouchableOpacity style={{ flex: 1, height: "70%", justifyContent: "center", backgroundColor: color.HIGHLIGHT }}>
+                {/* <Text style={[fontSize.CARD_CONTENT, { color: color.GREEN, textAlign: "center" }]}>EDIT</Text> */}
+                <Text style={{ fontSize: 35, color: color.GREEN, textAlign: "center" }}>✎</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={{ flex: 1, height: "100%", justifyContent: "center", backgroundColor: color.GREEN }}>
-                <Animated.Text style={[fontSize.CARD_TITLE, { color: color.TERTIARY_DARK, textAlign: "center", transform: [{ scale: scale }] }]}>ADD</Animated.Text>
+            <TouchableOpacity onPress={_pressAdd} style={{ flex: 1, height: "70%", justifyContent: "center", backgroundColor: color.GREEN }}>
+                {/* <Text style={[fontSize.CARD_CONTENT, { color: color.HIGHLIGHT, textAlign: "center" }]}>ADD</Text> */}
+                <Text style={{ fontSize: 35, color: color.HIGHLIGHT, textAlign: "center" }}>+</Text>
             </TouchableOpacity>
         </Animated.View>
     )
@@ -55,7 +49,7 @@ const RightAction = ({ progress, dragX }) => {
 // Formats data into a grid
 const formatData = (data, numColumns) => {
     let numberOfElements = data.length;
-    while (numberOfElements != 6) {
+    while (numberOfElements < 6) {
         numberOfElements++;
         if (numberOfElements !== 6) {
             data.push({ Type: "Empty" });
@@ -63,72 +57,89 @@ const formatData = (data, numColumns) => {
             data.push({ Type: "Last" });
         }
     }
+
+    if (numberOfElements >= 6) {
+        data[5] = { Type: "Last" };
+
+        for (let i = 6; data.length > 6; i++) {
+            data.pop();
+        }
+    }
     return data;
 };
 
-// Formats data into a grid
+// Formats data into a grid when editing
 const formatDataTwo = (data, numColumns) => {
     const fullRows = Math.floor(data.length / numColumns);
 
     let fillInLastRow = data.length - (fullRows * numColumns);
-    while (fillInLastRow != numColumns && fillInLastRow !== 0) {
+    for (fillInLastRow; fillInLastRow != numColumns && fillInLastRow !== 0; fillInLastRow++) {
         data.push({ Type: "Empty" });
-        fillInLastRow++;
     }
     return data;
 }
 
 class WorkoutCard extends Component {
     // Extracts index from data array and uses as key for muscle
-    _keyExtractor = (index) => {
-        return index
+    _keyExtractor = (item, index) => {
+        return item.Name
     };
 
     render() {
-        const { exercises, workoutName, isEditing } = this.props;
+        const { exercises, workoutName, isEditing, _pressEdit, _pressAdd } = this.props;
         return (
-            // <Swipeable containerStyle={{ width: "100%", height: "100%", overflow: "visible" }}
-            //     renderLeftActions={(progress, dragX) => <DeleteAction progress={progress} dragX={dragX} />}
-            //     renderRightActions={(progress, dragX) => <RightAction progress={progress} dragX={dragX} />}
-            // >
-                <View style={{ flex: 1, marginBottom: 10, overflow: "visible", justifyContent: "center" }}>
-                    <View style={[isEditing ? { height: "100%" } : { height: "70%" }, styles.workoutCardContainer]}>
-                        <View style={styles.workoutCardBackground}>
-                            {/* Start Workout Card */}
-                            <View style={styles.workoutCardRow}>
-                                {/* Start Workout Title */}
-                                <View style={styles.workoutCardTitle}>
-                                    {
-                                        isEditing ?
-                                            <TextInput style={[fontSize.CARD_CONTENT, styles.title]} placeholder="Day" placeholderTextColor={color.GREY} maxLength={9} autoCapitalize="words" />
-                                            :
-                                            <Text style={[fontSize.CARD_TITLE, styles.title]} numberOfLines={1}>Next Workout</Text>
-                                    }
-                                    <TextInput style={[fontSize.CARD_CONTENT, styles.workoutName]} placeholder="Name" placeholderTextColor={color.GREY} defaultValue={workoutName} maxLength={20} autoCapitalize="words" />
-                                </View>
-                                {/* End Workout Title */}
-
-                                {/* Start Muscle Groups */}
-                                <FlatList
-                                    numColumns="3"
-                                    key={workoutName}
-                                    style={styles.muscleGroup}
-                                    keyExtractor={this._keyExtractor}
-                                    data={isEditing ? formatDataTwo(exercises, 3) : formatData(exercises, 3)}
-                                    contentContainerStyle={{ justifyContent: "center", alignContent: "center", padding: 10 }}
-                                    renderItem={({ item, index }) =>
-                                        <Muscle key={index} containerStyle={styles.muscleContainer} muscleName={item.Type} />
-                                    }
-                                />
-                                {/* End Muscle Groups */}
-                            </View>
-                            {/* End Workout Card */}
-                        </View>
-                    </View>
-                </View>
-            // </Swipeable>
+            isEditing ?
+                <Swipeable containerStyle={{ flex: 1, marginBottom: 10, overflow: "visible" }}
+                    renderLeftActions={(progress, dragX) => <DeleteAction progress={progress} dragX={dragX} />}
+                    renderRightActions={(progress, dragX) => <RightAction progress={progress} dragX={dragX} _pressAdd={_pressAdd} />}
+                >
+                    <CardContent data={this.props} />
+                </Swipeable>
+                :
+                <CardContent data={this.props} />
         )
     }
+}
+
+const CardContent = (props) => {
+    const { exercises, workoutName, isEditing } = props.data;
+    return (
+        <View style={[isEditing ? { flex: 1 } : { flex: .7, }, styles.workoutCardContainer]}>
+            <View style={styles.workoutCardBackground}>
+                {/* Start Workout Card */}
+                <View style={styles.workoutCardRow}>
+                    {/* Start Workout Title */}
+                    <View style={styles.workoutCardTitle}>
+                        {
+                            isEditing ?
+                                <TextInput style={[fontSize.CARD_CONTENT, styles.title]} placeholder="Day" placeholderTextColor={color.GREY} maxLength={9} autoCapitalize="words" />
+                                :
+                                <Text style={[fontSize.CARD_TITLE, styles.title]} numberOfLines={1}>Next Workout</Text>
+                        }
+                        <TextInput style={[fontSize.CARD_CONTENT, styles.workoutName]} placeholder="Name" placeholderTextColor={color.GREY} defaultValue={workoutName} maxLength={20} autoCapitalize="words" />
+                    </View>
+                    {/* End Workout Title */}
+
+                    {/* Start Muscle Groups */}
+                    <FlatList
+                        numColumns="3"
+                        style={styles.muscleGroup}
+                        keyExtractor={(item, index) => item.Name + index}
+                        data={isEditing ? formatDataTwo(exercises, 3) : formatData(exercises, 3)}
+                        contentContainerStyle={{ justifyContent: "center", alignContent: "center" }}
+                        renderItem={({ item, index }) => {
+                            return (
+                                <Muscle key={index} containerStyle={styles.muscleContainer} muscleName={item.Type} />
+                            )
+                        }
+                        }
+                    />
+                    {/* End Muscle Groups */}
+                </View>
+                {/* End Workout Card */}
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -207,20 +218,22 @@ const styles = StyleSheet.create({
     },
     muscleGroup: {
         flex: 1,
-        width: "100%",
         alignSelf: "center",
     },
     deleteWorkoutRow: {
         width: 80,
         height: "100%",
+        marginRight: "-5%",
+        flexDirection: "row",
+        alignItems: "center",
         justifyContent: "center",
-        backgroundColor: color.RED
     },
     rightActionsRow: {
         width: 120,
         height: "100%",
+        marginLeft: "-5%",
         flexDirection: "row",
-        alignItems: "flex-end",
+        alignItems: "center",
         justifyContent: "center",
     },
 });
