e543c4175b15e37e3840974abc842498908e564c
Victor Ragojos <vhsvragojos@gmail.com>
2019-07-10 17:37:02 -0700

    optimized performance of View Component

--- Components/Views/Card.js
+++ Components/Views/Card.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import { color } from '../config/colors';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import { TouchableOpacity, StyleSheet } from 'react-native';
 
-const Card = (props) => {
-    const { containerStyle, addContStyle, isSwipeable, leftActions, rightActions, isTouchable, _press, children } = props;
-    return (
-        isTouchable ?
-            <TouchableOpacity style={[addContStyle, containerStyle ? containerStyle : styles.cardContainer]} onPress={_press}>
+class Card extends React.PureComponent {
+    render() {
+        const { containerStyle, addContStyle, isTouchable, _press, children } = this.props;
+        return (
+            <TouchableOpacity disabled={!isTouchable} style={[addContStyle, containerStyle ? containerStyle : styles.cardContainer]} onPress={_press}>
                 {children}
             </TouchableOpacity>
-            :
-            isSwipeable ?
-                <Swipeable containerStyle={{ width: "100%", height: "100%", overflow: "visible" }} renderLeftActions={leftActions} renderRightActions={rightActions}>
-                    <View style={[addContStyle, containerStyle ? containerStyle : styles.cardContainer]}>
-                        {children}
-                    </View>
-                </Swipeable>
-                :
-                <View style={[addContStyle, containerStyle ? containerStyle : styles.cardContainer]}>
-                    {children}
-                </View>
-    )
+        )
+    }
 }
 
 const styles = StyleSheet.create({
