f1344fc0154bbfb37a048d10c86e791e8185aa43
Victor Ragojos <vhsvragojos@gmail.com>
2019-07-12 00:25:07 -0700

    App now awaits for required Assets and fonts

--- Components/Index.js
+++ Components/Index.js
@@ -1,28 +1,42 @@
 import Nav from './Nav/Nav';
 import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import { AppLoading } from 'expo';
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { useScreens } from 'react-native-screens';
-import { isFontLoaded } from '../Redux/Actions/initLoadAction';
+import { isAssetLoaded } from '../Redux/Actions/initLoadAction';
 
 class Index extends Component {
     async componentDidMount() {
         useScreens();
-        await Font.loadAsync({
-            'Roboto-Regular': require("../assets/fonts/Roboto-Regular.ttf"),
-            'Roboto-Medium': require("../assets/fonts/Roboto-Medium.ttf"),
-            'Roboto-Bold': require("../assets/fonts/Roboto-Bold.ttf")
-        })
 
-        this.props.dispatch(isFontLoaded(true));
+        await Promise.all([
+            Asset.loadAsync([
+                require('../assets/Abs.png'),
+                require('../assets/Arms.png'),
+                require('../assets/Back.png'),
+                require('../assets/Legs.png'),
+                require('../assets/Save.png'),
+                require('../assets/Chest.png'),
+                require('../assets/Shoulders.png'),
+            ]),
+            Font.loadAsync({
+                'Roboto-Regular': require("../assets/fonts/Roboto-Regular.ttf"),
+                'Roboto-Medium': require("../assets/fonts/Roboto-Medium.ttf"),
+                'Roboto-Bold': require("../assets/fonts/Roboto-Bold.ttf")
+            })
+        ]);
+
+        return this.props.dispatch(isAssetLoaded(true));
     }
 
     render() {
         return (
             // TODO: Create a loading screen
-            this.props.isFontLoaded ?
+            this.props.isAssetLoaded ?
                 <Nav />
-                : null
+                : <AppLoading />
         );
     }
 }
