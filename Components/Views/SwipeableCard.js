f8b5de53f74b952a170d455aa65fd4258f655a8d
Victor Ragojos <vhsvragojos@gmail.com>
2019-07-14 16:36:14 -0700

    removed shadow

--- Components/Views/SwipeableCard.js
+++ Components/Views/SwipeableCard.js
@@ -4,8 +4,7 @@ import { View, StyleSheet } from 'react-native';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 const SwipeableCard = (props) => {
-    const { children, isOvershootableLeft, isOvershootableRight, leftAction, rightAction,
-        containerStyle } = props;
+    const { children, isOvershootableLeft, isOvershootableRight, leftAction, rightAction, containerStyle } = props;
     return (
         <Swipeable
             style={styles.swipeContainer}
@@ -24,27 +23,17 @@ const SwipeableCard = (props) => {
 const styles = StyleSheet.create({
     swipeContainer: {
         flex: 1,
-        overflow: "visible",
     },
     viewContainer: {
         flex: 1,
         height: 120,
         width: "90%",
         borderRadius: 5,
+        overflow: "visible",
         paddingVertical: 10,
         alignSelf: "center",
         paddingHorizontal: 20,
         backgroundColor: color.SECONDARY_DARK,
-
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowOpacity: 0.29,
-        shadowRadius: 4.65,
-
-        elevation: 7,
     }
 })
 
